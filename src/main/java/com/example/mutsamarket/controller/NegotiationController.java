9f36ca668ba7790ea1c6cbc224c3726b53bb9c7d
yudaeon <daeon03050@gmail.com>
2023-07-05 11:25:01 +0900

    구매 확정 상태 오류 수정

--- src/main/java/com/example/mutsamarket/controller/NegotiationController.java
+++ src/main/java/com/example/mutsamarket/controller/NegotiationController.java
@@ -48,21 +48,22 @@ public class NegotiationController {
             @PathVariable("proposalId") Long proposalId,
             @RequestBody NegotiationDto negotiationDto
     ) {
-        //제안수정 기능 추가
+        // 제안수정 기능 추가
         if (negotiationDto.getStatus() == null) {
             this.service.updateNegotiation(itemId, proposalId, negotiationDto);
             ResponseDto response = new ResponseDto();
             response.setMessage("제안이 수정되었습니다.");
             return ResponseEntity.ok(response);
 
-        } //구매확정 기능 추가
-        else if (negotiationDto.getStatus().equals("확정")){
+        }
+        //구매 확정 기능 수정
+        else if (negotiationDto.getStatus() != null && negotiationDto.getStatus().equals("확정")) {
             this.service.confirmNegotiation(itemId, proposalId, negotiationDto);
             ResponseDto response = new ResponseDto();
             response.setMessage("구매가 확정되었습니다.");
             return ResponseEntity.ok(response);
-
-        }   //수락, 거절 상태변경 기능 추가
+        }
+        //수락, 거절 상태변경 기능 추가
         else {
             this.service.acceptNegotiation(itemId, proposalId, negotiationDto);
             ResponseDto response = new ResponseDto();
@@ -77,7 +78,7 @@ public class NegotiationController {
     public ResponseDto deleteNegotiation(@PathVariable("itemId") Long itemId,
                                          @PathVariable("proposalId")Long proposalId,
                                          @RequestBody NegotiationDto dto){
-        service.deleteNegotiation(itemId, proposalId, dto);
+        this.service.deleteNegotiation(itemId, proposalId, dto);
         ResponseDto responseDto = new ResponseDto();
         responseDto.setMessage("제안을 삭제했습니다.");
         return responseDto;
