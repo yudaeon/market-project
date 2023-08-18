b24515ea6b83f56ca91cf9789ea133bfb784d5c6
yudaeon <daeon03050@gmail.com>
2023-08-18 13:29:33 +0900

    구매 확정 상태 오류 수정

---
+++ src/main/java/com/example/mutsamarket/exception/BadRequestException.java
@@ -0,0 +1,15 @@
+package com.example.mutsamarket.exception;
+
+public class BadRequestException extends Exception{
+    public BadRequestException() {
+        super();
+    }
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+
+    public BadRequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
